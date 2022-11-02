c7a9e0f409c203210b03d4e771af34233d291ae3
Malek-ALdesougi <malekaldesougi@gmail.com>
2022-11-02 23:10:10 +0200

    I have converted the class components into a functios components

--- bmi-calculator/src/form.js
+++ bmi-calculator/src/form.js
@@ -1,62 +1,54 @@
 import React from "react";
 import Result from "./result";
+import { useState } from "react";
 
-class Form extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {weight: '', height: ''};
-  
-      this.handleChange = this.handleChange.bind(this);
-    //   this.handleSubmit = this.handleSubmit.bind(this);
-    }
-  
-    handleChange(event) {
-      console.log(event.target.value);
-      this.setState({[event.target.name]: event.target.value})
-    }
-  
-    //////////////// NOT WORKING WAY//////////////
-    // handleSubmit(event) {
-    // //   alert('A name was submitted: ');
-    //   event.preventDefault();
-    //   console.log('onSumbit get fired');
+function Form(props){
+ 
+  const [weight , setWeight] = useState('');
+  const [height , setHeight] = useState('');
+  const [bmi , setBmi] = useState('');
+  const [flag, setFlag] = useState(false);
+ 
+  //HERE WE USE TO DO ANYTHING WE WANT WITH THE INPUTS LIKE DATABASE OR ECT...
+  function handleSubmit(e){
+    // alert('A weight was submitted:' + {weight} + 'and the height is ' + {height});
+    e.preventDefault();
+    console.log("onSumbit get fired");
 
-    //   //to clear the form after submit 
-    //   this.setState({weight: '', height: ''});
-    // }
+    let result = weight / height / height * 10000;
+    setBmi(result)
 
-    //HERE WE USE TO DO ANYTHING WE WANT WITH THE INPUTS LIKE DATABASE OR ECT...
-    handleSubmit = (e) => {
-        alert('A weight was submitted:' + this.state.weight + 'and the height is ' + this.state.height);
-        // e.preventDefault();
-        console.log('onSumbit get fired');
+    setFlag(true);
+  }
 
-        // <Result/>
-        localStorage.setItem('weight',this.state.weight)
-        localStorage.setItem('height' , this.state.height)
-
-       //to clear the form after submit 
-    this.setState({weight: '',height: '' })
-}
-  
-    render() {
-      return (
-        <form className="form" onSubmit={this.handleSubmit}>
+    return (
+      <div>
+        <form className="form" onSubmit={handleSubmit}>
           <label>
             Weight:
-            <input name="weight" type="text" value={this.state.weight} onChange={this.handleChange} />
+            <input
+              name="weight"
+              type="text"
+              value={weight}
+              onChange={(e) => setWeight(e.target.value)}
+            />
           </label>
 
           <label>
             Height:
-            <input name="height" type="text" value={this.state.height} onChange={this.handleChange} />
+            <input
+              name="height"
+              type="text"
+              value={height}
+              onChange={(e) => setHeight(e.target.value)}
+            />
           </label>
           <input className="button" type="submit" value="Calculate BMI" />
         </form>
-      );
-    }
+        {/* <p>This is the weight : {weight} And this is the height : {height}</p> */}
+        { (flag ? <Result result={bmi}/> : '')}
+      </div>
+    );
   }
 
 export default Form;
-
-
--- bmi-calculator/src/result.js
+++ bmi-calculator/src/result.js
@@ -1,55 +1,38 @@
 import React from "react";
 
-class Result extends React.Component {
-  constructor(props) {
-    super();
-  }
+function Result(props) {
 
-  swit(result) {
-    // switch (result) {
-    //   case (result <= 18.5):
-    //     return 'You are Underweight';
+  let myResult = props.result;
+  // let weight = props.weight;
+  // let height = props.height;
+  // let result = (weight / height / height) * 10000;
+  console.log(myResult);
 
-    //   case (result > 18.5 && result < 24.9):
-    //     return 'You are at the Normal weight';
-
-    //   case if(result >= 25 && result < 29.9):
-    //     return 'You are at the Overweight';
-
-    //   default:
-    //     return 'you are on the stage';
-    // }
-    if (result <= 18) {
+  // this.state = { weight: this.props.weight, height: this.props.height };
+  // console.log(this.props.weight);
+  function swit(value) {
+    if (value <= 18) {
       return "You are Underweight";
     }
-    if (result > 18.5 && result < 24.9) {
+    if (value > 18.5 && value < 24.9) {
       return "You are at the Normal weight";
     }
-    if (result >= 25 && result < 29.9) {
+    if (value >= 25 && value < 29.9) {
       return "You are at the Overweight";
     } else {
       return "You are at the Opacity stage !! ";
     }
   }
 
-  render() {
-    let weight = localStorage.getItem("weight");
-    let height = localStorage.getItem("height");
-
-    let result = (weight / height / height) * 10000;
-    console.log(result);
-    if (result) {
-      return (
-        <div className="div">
-          <h5>You Weight is {weight}</h5>
-          <h5>You Height is {height}</h5>
-          <h3>Your Body Mass Index (BMI) is :{result.toFixed(2)}</h3>
-          <h1 className="color">Result :</h1>
-          <h1>{this.swit(result)}</h1>
-        </div>
-      );
-    }
-  }
+  return (
+    <div className="div">
+      {/* <h5>You Weight is {weight}</h5>
+      <h5>You Height is {height}</h5> */}
+      <h3>Your Body Mass Index (BMI) is : {myResult.toFixed(2)}</h3>
+      <h1 className="color">Result : </h1>
+      <h1>{swit(myResult)}</h1>
+    </div>
+  );
 }
 
 export default Result;
